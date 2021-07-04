a6d74ec16fe0fe87b07808c1cda6f56a3f28e086
Abigael <abigael.desandi@gmail.com>
2021-07-04 13:58:07 +0200

    ajout vérification email et mdp

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,11 @@ require('dotenv').config();
 
 // fonction signup pour créer un utilisateur - testée ok 
 exports.signup = (req, res, next) => {
-    if (ValidationMail(req.body.password)) {
+    var emailReg = new RegExp(/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i); // regex qui vérifie que l'email est cohérent - protection supp.
+    var validEmail = emailReg.test(req.body.email);
+    var mdpReg = new RegExp(/^[\w-\.]{7,8}$/i); // regex qui vérifie que le mdp est cohérent - protection supp.
+    var validMdp = mdpReg.test(req.body.password);
+    if (validEmail && validMdp) { // si email entré par utilisateurET le mdp correspondent au regex on passe à la suite des actions/fonctions
         bcrypt.hash(req.body.password, 10) 
         // fonction pour crypter le mdp, on lui passe en paramètre, 10 est le nombre de tour pour le hachage (on ne veut pas que ce soit trop long)
         .then(hash => { // hash est le résultat de bcrypt.hash (on l'appelle comme on veut)
@@ -20,51 +24,45 @@ exports.signup = (req, res, next) => {
         })
         .catch(error => res.status(500).json({ error }));
     } else {
-        return error;
+        throw 'Adresse email ou mot de passe non valide !';
     }
 };
 
 // fonction login pour se connecté une fois l'utilisateur créé - testée ok 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email }) // on cherche dans la bdd l'email correspondant à la requête
-    .then(user => { // le résultat de findOne renvoi un valeur booléenne qu'on va appelé user
-    if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-        // si user est faux c'est qu'on ne la pas trouvé dans la bdd donc on renvoi un mess
-    }
-    bcrypt.compare(req.body.password, user.password) // si on a trouvé user dans la bdd on compare les mdp
-    // comparaison de mdp de la requête et de la bdd, ceci renvoi un résultat boléen qu'on appelle valid
-        .then(valid => {
-            if (!valid) { // s'il n'y a pas de correspondance valide est faux et on renvoi un message d'erreur
-                return res.status(401).json({ error: 'Mot de passe incorrect !' });
-            }
-            res.status(200).json({ // si valid est vrai il y a concordance 
-                // en cas de réussite on renvoi au front ce qui l'attent : un id et un token
-                userId: user._id,
-                token: jwt.sign(
-                    // grâce à jwt.sign on crée un token de connexion qui sera vérifié
-                    { userId: user._id }, // on encode le userId en début de token (pour affiliation à un objet lui seul pourra le modifier)
-                    process.env.TOKEN, // RANDOM_TOKEN_SECRET - pahdtcps521199cjneyslfh1545sljfsss1145 -> celui-ci sera encodé !
-                    { expiresIn: '6 hours' } // le token n'est plus valide au-delà de 6h
-                )
-            });
+    var emailReg = new RegExp(/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i);
+    var validEmail = emailReg.test(req.body.email);
+    var mdpReg = new RegExp(/^[\w-\.]{7,8}$/i); 
+    var validMdp = mdpReg.test(req.body.password);
+    if (validEmail && validMdp) { 
+        User.findOne({ email: req.body.email }) // on cherche dans la bdd l'email correspondant à la requête
+        .then(user => { // le résultat de findOne renvoi un valeur booléenne qu'on va appelé user
+        if (!user) {
+            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+            // si user est faux c'est qu'on ne la pas trouvé dans la bdd donc on renvoi un mess
+        }
+        bcrypt.compare(req.body.password, user.password) // si on a trouvé user dans la bdd on compare les mdp
+        // comparaison de mdp de la requête et de la bdd, ceci renvoi un résultat boléen qu'on appelle valid
+            .then(valid => {
+                if (!valid) { // s'il n'y a pas de correspondance valide est faux et on renvoi un message d'erreur
+                    return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                }
+                res.status(200).json({ // si valid est vrai il y a concordance 
+                    // en cas de réussite on renvoi au front ce qui l'attent : un id et un token
+                    userId: user._id,
+                    token: jwt.sign(
+                        // grâce à jwt.sign on crée un token de connexion qui sera vérifié
+                        { userId: user._id }, // on encode le userId en début de token (pour affiliation à un objet lui seul pourra le modifier)
+                        process.env.TOKEN, // RANDOM_TOKEN_SECRET - pahdtcps521199cjneyslfh1545sljfsss1145 -> celui-ci sera encodé !
+                        { expiresIn: '6 hours' } // le token n'est plus valide au-delà de 6h
+                    )
+                });
+            })
+            .catch(error => res.status(500).json({ error }));
         })
-        .catch(error => res.status(500).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(500).json({ error }));;
+    } else {
+        throw 'Adresse email ou mot de passe non valide !';
+    }
 };
 
-function ValidationMail(mail) {
-	var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-	if (mail.match(regex))
-	{
-		alert("Adresse mail valide");
-        return true;
-	}
-	else
-	{
-		alert("Adresse mail non valide");
-        return false;
-	}
-}
